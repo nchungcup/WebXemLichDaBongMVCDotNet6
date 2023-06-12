6c7f70058b239ddd6c11a70b8d19e8d34c53b4c6
Cụp Đây <nguyencanhhungzed@gmail.com>
2023-06-13 02:18:21 +0700

    //

--- DACN_WebXemLichDaBong/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DACN_WebXemLichDaBong.Models
-{
-	public class ErrorViewModel
-	{
-		public string RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
